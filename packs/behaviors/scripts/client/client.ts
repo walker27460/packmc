13a78585a6209ffe9f02bae643c74738e7c097af
Yuui Tanabe <m19102@g.metro-cit.ac.jp>
2020-05-24 06:47:02 +0000

    yes

---
+++ packs/behaviors/scripts/client/client.ts
@@ -0,0 +1,48 @@
+/// <reference types="minecraft-scripting-types-client" />
+
+namespace Client {
+	const system = client.registerSystem(0, 0);
+
+	// Setup which events to listen for
+	system.initialize = function() {
+		// Register any events you will send to the client
+		const eventDataDefaults = {narf: false}
+		system.registerEventData("example:pinky", eventDataDefaults)
+
+		// Register any components you will attach to game objects
+		// system.registerComponent(...)
+
+		// Set up any events you wish to listen to
+		// system.listenForEvent(...);
+
+		// Enable full logging, useful for seeing errors, you will probably want to disable this for
+		// release versions of your scripts.
+		// Generally speaking it's not recommended to use broadcastEvent in initialize, but for configuring logging it's fine.
+		const scriptLoggerConfig = system.createEventData(SendToMinecraftClient.ScriptLoggerConfig);
+		scriptLoggerConfig.data.log_errors = true;
+		scriptLoggerConfig.data.log_information = true;
+		scriptLoggerConfig.data.log_warnings = true;
+		system.broadcastEvent(SendToMinecraftClient.ScriptLoggerConfig, scriptLoggerConfig);
+	}
+
+	let firstTick = true;
+	// per-tick updates
+	system.update = function() {
+		// Any logic that needs to happen every tick on the client.
+		if (firstTick) {
+			firstTick = false;
+
+			//set up chat event data object
+			let chatEventData = system.createEventData("minecraft:display_chat_event");
+			chatEventData.data.message = "What are we going to do tonight Server?";
+
+			system.broadcastEvent(SendToMinecraftClient.DisplayChat, chatEventData);
+			
+			// You must create the event data this way and then set your properties before you broadcast
+			const eventData = system.createEventData("example:pinky");
+			eventData.data.narf = true;
+
+			system.broadcastEvent("example:pinky", eventData);
+		}
+	}
+}
---
+++ packs/behaviors/scripts/server/server.ts
@@ -0,0 +1,40 @@
+/// <reference types="minecraft-scripting-types-server" />
+
+namespace Server {
+	var system = server.registerSystem(0, 0);
+
+	// Setup which events to listen for
+	system.initialize = function () {
+		// Register any events you will send to the client
+		// system.registerEventData(...)
+
+		// Register any components you will attach to game objects
+		// system.registerComponent(...)
+
+		// Set up any events you wish to listen to
+		system.listenForEvent("example:pinky", receivePinkyMessage);
+
+		// Enable full logging, useful for seeing errors, you will probably want to disable this for
+		// release versions of your scripts.
+		// Generally speaking it's not recommended to use broadcastEvent in initialize, but for configuring logging it's fine.
+		const scriptLoggerConfig = system.createEventData(SendToMinecraftServer.ScriptLoggerConfig);
+		scriptLoggerConfig.data.log_errors = true;
+		scriptLoggerConfig.data.log_information = true;
+		scriptLoggerConfig.data.log_warnings = true;
+		system.broadcastEvent(SendToMinecraftServer.ScriptLoggerConfig, scriptLoggerConfig);
+	}
+
+	// per-tick updates
+	system.update = function() {
+		// Any logic that needs to happen every tick on the server.
+	}
+
+	function receivePinkyMessage(parameters: IEventData<{narf: boolean}>) {
+		if (parameters.data.narf) {
+			var displayChatEvent = system.createEventData(SendToMinecraftServer.DisplayChat);
+			displayChatEvent.data.message = "The same thing we do every night Client. TRY TO TAKE OVER THE WORLD.";
+			system.broadcastEvent(SendToMinecraftServer.DisplayChat, displayChatEvent);
+		}
+	}
+}
+
